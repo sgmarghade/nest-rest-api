9dace7509aac43311972c303394aca584326f9a3
Swapnil Marghade <sgmarghade@Swapnil-Marghade-MD4NN213Q3.local>
2024-04-25 13:39:28 +0530

    Add throttle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 60000, //1 min
+        limit: 3, //3 req / 1 min
+      },
+    ]),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
